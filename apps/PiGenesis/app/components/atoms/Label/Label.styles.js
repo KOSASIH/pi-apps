4a79d6bf2744eb3491502328be394467623cc975
KOSASIH <kosasihg88@gmail.com>
2024-06-15 09:40:40 +0700

    Create Label.styles.js

---
+++ apps/PiGenesis/app/components/atoms/Label/Label.styles.js
@@ -0,0 +1,11 @@
+import { css } from 'styled-components';
+
+const LabelStyles = css`
+  .label {
+    font-size: 16px;
+    font-weight: 400;
+    margin-bottom: 4px;
+  }
+`;
+
+export default LabelStyles;
